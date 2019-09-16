39223035bad864e8adbb2eb982c364f0a44b5569
Juliano Krindges <julianokrindges@gmail.com>
2019-09-15 23:07:59 -0300

    feat: topbar component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react'
+
+import './styles/styles.css'
 import Header from './components/Header/index'
 
 function App() {
   return (
-
+    
+    <Header />
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import './style.css'
 
 class Header extends Component {
   render(){
     return(
       <header>
-        
+        <strong>facebook</strong>
+        <small>Meu perfil</small>
       </header>
     );
   }
