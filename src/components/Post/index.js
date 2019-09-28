06ca42632ba4545c4ce644a790ce49bd3ca00830
Juliano Krindges <julianokrindges@gmail.com>
2019-09-28 18:34:24 -0300

    feat: small changes on css

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import ImgAvatar from '../../assets/avatar2.png'
 
 function PostHeader ({author, date}) {
     return (
         <div className="post-header">
           <img src={author.avatar}/>
           <div className="header-details">
-            <strong>{author.name}</strong>
-            <small>{date}</small>
+            <span>{author.name}</span>
+            <span>{date}</span>
           </div>
         </div>
     )
@@ -18,7 +18,7 @@ function PostComments ({comments}) {
     <div className="comments">
           <div className="divider" />
             <div className="post-comment">
-              <img src='' alt=""/>
+              <img src={ImgAvatar} alt=""/>
               <p><strong>Diego Fernandes</strong> A Rocketseat está sempre em busca de novos membros 
                 para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, 
                 inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. 
