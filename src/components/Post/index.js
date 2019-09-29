49f76210b7c2f780bb4c4e691f15be6b8ad9f4d3
Juliano Krindges <juliano9krindges@hotmail.com>
2019-09-29 00:39:45 -0300

    chore: add data from state

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ImgAvatar from '../../assets/avatar2.png'
 
 function PostHeader ({author, date}) {
     return (
@@ -17,14 +16,12 @@ function PostComments ({comments}) {
   return(
     <div className="comments">
           <div className="divider" />
-            <div className="post-comment">
-              <img src={ImgAvatar} alt=""/>
-              <p><strong>Diego Fernandes</strong> A Rocketseat está sempre em busca de novos membros 
-                para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, 
-                inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. 
-                Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, 
-                pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real) </p>
+          {comments.map(comment => (
+            <div key={comment.id}className="post-comment">
+              <img src={comment.author.avatar} alt={comment.name}/>
+              <p><strong>{comment.author.name}</strong> {comment.content} </p>
             </div>
+          ))}
     </div>
   )
 }
@@ -33,8 +30,8 @@ function Post ({author, date, content, comments}) {
   return(
     <div className="post">
       <PostHeader author={author} date={date}/>
-        <p className="post-text">Pessoal, alguem sabe se a Rocketseat está contratando?</p>
-      <PostComments /> 
+        <p className="post-text">{content}</p>
+      <PostComments comments={comments}/> 
     </div>
  
   )
